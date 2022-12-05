87e1fa19e8885320e271287c61ac350b4c3f96c8
LamphereRobotics6566 <98193968+LamphereRobotics6566@users.noreply.github.com>
2022-12-04 22:02:22 -0500

    aoc2022 day01

---
+++ day01/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using(StreamReader file = new StreamReader("Sample.txt")){
+
+ string? ln;  
+  int calCount = 0;
+List<int> calList = new List<int>();
+ int largestNumber = 0;
+ int numSum = 0;
+ 
+ while ((ln = file.ReadLine()) != null) {  
+    if(ln.Length == 0){
+          calList.Add(calCount);
+         if(calCount > largestNumber){
+           largestNumber = calCount;
+         }
+          calCount = 0;
+   } else {
+      calCount += int.Parse(ln);
+   }
+ }  
+ calList.Sort();
+ for(int i = calList.Count() - 3; i <  calList.Count(); i++){
+    Console.WriteLine(calList[i]);
+    numSum += calList[i];
+ }
+ Console.WriteLine(numSum);
+ file.Close();  
+} 
